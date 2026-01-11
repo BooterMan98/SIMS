042315ae25db793683878e4a0da7e1fdb6dcaa12
César Paulangleo <20160120+BooterMan98@users.noreply.github.com>
2026-01-11 18:50:35 -0300

    refactor: Extracted methods from CommandsArgs.
    - Extracted code form CommandsArgs constructor into many private static methods for better readability.
    - Added some comments to CommandsArgs

--- Simple-Inventory-Management-System/CommandsArgs.cs
+++ Simple-Inventory-Management-System/CommandsArgs.cs
@@ -1,5 +1,9 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 
+/// <summary>
+/// Struct that contains all elements received from a command.
+/// </summary>
 readonly struct CommandArgs
 {
 [SetsRequiredMembers]
@@ -8,66 +12,97 @@ readonly struct CommandArgs
     var commandStrArgs = commandStr.Split(' ');
 
     Action = GetCommandFromString(commandStrArgs[0]);
+    ArgumentsMissing = GetMissingArgs(commandStrArgs);
 
-    var argsCount = commandStrArgs.Count();
-    var missingArgs = MissingArgs.name 
-    | MissingArgs.price 
-    | MissingArgs.quantity 
-    | MissingArgs.newName;
-
-    if (argsCount >= 2)
-    {
-      string potentialName = commandStrArgs[1];
-      Name = potentialName;
-      missingArgs ^= MissingArgs.name;
-    }
-    if (argsCount >= 3)
-    {
-      if (int.TryParse(commandStrArgs[2], out int potentialPrice))
-      {
-        Price = potentialPrice;
-        missingArgs ^= MissingArgs.price;
-      }
-    }
-    if (argsCount >= 4)
-    {
-      if (int.TryParse(commandStrArgs[3], out int potentialQuantity))
-      {
-        Quantity = potentialQuantity;
-        missingArgs ^= MissingArgs.quantity;
-      }
-    }
-    if (argsCount >= 5)
-    {
-      string potentialNewName = commandStrArgs[4];
-      NewName = potentialNewName;
-      missingArgs ^= MissingArgs.newName;
-    }
-
-
-    ArgumentsMissing = missingArgs;
-
-    switch (Action) {
-      case Command.Add:
-        {
-          break;
-        }
-      case Command.Exit or Command.Unknown: 
-        {
-          break;
-        }
-
-    }
-
+    Name = GetStringFromArgs(commandStrArgs, MissingArgs.Name ,ArgumentsMissing);
+    Price = GetIntFromArgs(commandStrArgs, MissingArgs.Price ,ArgumentsMissing);
+    Quantity = GetIntFromArgs(commandStrArgs, MissingArgs.Quantity ,ArgumentsMissing);
+    NewName = GetStringFromArgs(commandStrArgs, MissingArgs.NewName ,ArgumentsMissing);
   }
-
 
   public required Command Action { get; init; }
   public required string? Name { get; init; }
   public required int? Price { get; init; }
   public required int? Quantity { get; init; }
   public string? NewName { get; init; }
+  /// <summary>
+  /// Arguments not present in this command.
+  /// </summary>
   public MissingArgs ArgumentsMissing { get; init; }
+
+  private static string? GetStringFromArgs(string[] strArgs, MissingArgs argToFind,MissingArgs missingArgs)
+  {
+    if (missingArgs.HasFlag(argToFind))
+    {
+      return null;
+    }
+
+    return argToFind switch
+    {
+      MissingArgs.Name => strArgs[1],
+      MissingArgs.NewName => strArgs[4],
+      _ => throw new ArgumentException(message: "Invalid argument for getting a string")
+    };
+  }
+
+  private static int? GetIntFromArgs(string[] strArgs, MissingArgs argToFind,MissingArgs missingArgs)
+  {
+    if (missingArgs.HasFlag(argToFind))
+    {
+      return null;
+    }
+    string possibleInt;
+    switch (argToFind)
+    {
+      case MissingArgs.Price: 
+      {
+        possibleInt = strArgs[2];
+        break;
+      }
+      case MissingArgs.Quantity:
+      {
+        possibleInt = strArgs[3];
+        break;
+      }
+      default: 
+      {
+        throw new ArgumentException(message: "Invalid argument for getting a string");
+      }
+    };
+
+    return int.TryParse(possibleInt, out var result) ? result : null;
+
+  }
+
+
+  private static MissingArgs GetMissingArgs(string[] commandStrArgs)
+  {
+    var argsCount = commandStrArgs.Count();
+    var missingArgs = MissingArgs.Name
+    | MissingArgs.Price
+    | MissingArgs.Quantity
+    | MissingArgs.NewName;
+
+    if (argsCount >= 2)
+    {
+      missingArgs ^= MissingArgs.Name;
+    }
+    if (argsCount >= 3)
+    {
+      missingArgs ^= MissingArgs.Price;
+    }
+    if (argsCount >= 4)
+    {
+      missingArgs ^= MissingArgs.Quantity;
+    }
+    if (argsCount >= 5)
+    {
+      missingArgs ^= MissingArgs.NewName;
+    }
+
+    return missingArgs;
+  }
+
 
 
   private static Command GetCommandFromString (string commandStr)
